cff499a725986016711ac1dd2a1d66d5d1998ba9
Matthew Akram <mazfh85246@gmail.com>
2021-11-06 13:02:43 +0100

    implemented continuous_vector_pool.py. The code can be a lot more readable. I can fix that later

--- active_learning_ts/pools/continuous_vector_pool.py
+++ active_learning_ts/pools/continuous_vector_pool.py
@@ -29,6 +29,8 @@ class ContinuousVectorPool(Pool):
     def __init__(self, dim, ranges: List[List[Tuple[float, float]]]):
         self.shape = (dim,)
         self.ranges = []
+        self.sizes = []
+        self.start_values = []
 
         for dimension in ranges:
             self.ranges.append([ContinuousVectorPool.Range(a, b) for a, b in dimension])
@@ -44,35 +46,70 @@ class ContinuousVectorPool(Pool):
             for i in range(len(dim) - 1):
                 if dim[i].intersects(dim[i + 1]):
                     raise ValueError("Intersection of intervals must be empty")
-                self.total_sizes[j] += dim[i].size
-            self.total_sizes[j] += dim[len(dim) - 1].size
+                self.total_sizes[j] += float(dim[i].size)
+            self.total_sizes[j] += float(dim[len(dim) - 1].size)
 
-    #@tf.function
+        # data from range objects allowed to sort and check the data. We now need to convert it back to ints, so we
+        # can work with tf
+        for i in range(len(ranges)):
+            self.sizes.append([])
+            self.start_values.append([])
+            for j in range(len(ranges[i])):
+                current_range = self.ranges[i][j]
+                self.sizes[i].append(float(current_range.size))
+                self.start_values[i].append(float(current_range.lower))
+
+    @tf.function
     def get_element(self, element: tf.Tensor) -> tf.Tensor:
+        """
+        Gets a value #TODO: this doc
+        :param element:
+        :return:
+        """
+        # please do not try to read this code
+
         indices = tf.unstack(element)
 
         out = []
 
-        # avoids tf issues
+        # declare all variables at the beginning in order to avoid tf issues
         next_out = None
+        start_value_list_iterator = iter(self.start_values)
+        size_list_iterator = iter(self.sizes)
+        total_size_iterator = iter(self.total_sizes)
+        total_covered = 0.0
+        next_total = 0.0
+        correct_offset = 0.0
+        start_value_iterator = None
+        size_iterator = None
 
-        for i in range(0, len(indices)):
-            index = indices[i] * self.total_sizes[i]
+        def loop_body(iterator, body_index, body_total_covered, body_next_total, body_correct_offset, body_size_list,
+                      body_start_value):
+            body_total_covered = body_next_total
+            body_next_total += tf.gather(body_size_list, iterator)
+            body_correct_offset = tf.gather(body_start_value, iterator)
+
+            return iterator + 1, body_index, body_total_covered, body_next_total, body_correct_offset, body_size_list, body_start_value
+
+        for index in indices:
+            index = index * next(total_size_iterator)
 
             total_covered = 0.0
             next_total = 0.0
-            correct_range = None
-            for current_range in self.ranges[i]:
-                next_total += current_range.size
-                if next_total >= index:
-                    correct_range = current_range
-                    break
-                total_covered = next_total
+            correct_offset = 0.0
+            start_value_list = next(start_value_list_iterator)
+            size_list = next(size_list_iterator)
+
+            j, i, total_covered, y, correct_offset, l1, l2 = tf.while_loop(
+                lambda j, i, x, y, z, l1, l2: tf.math.less_equal(y, i),
+                loop_body,
+                [0, index, total_covered, next_total, correct_offset, size_list, start_value_list],
+                parallel_iterations=10)
 
             # at this point, we have located the correct range, we just need to find the percentage of this range in
             # which the given point lies
 
-            next_out = index - total_covered + correct_range.lower
+            next_out = index - total_covered + correct_offset
 
             out.append(next_out)
 
