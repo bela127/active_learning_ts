cd92b282cce901f0c349f024af54a208881757c4
bela127 <bhb127@outlook.de>
2022-02-11 13:33:32 +0100

    Adding Types

--- active_learning_ts/pool.py
+++ active_learning_ts/pool.py
@@ -1,4 +1,4 @@
-from typing import Protocol, List
+from typing import Protocol, List, Tuple
 
 import tensorflow as tf
 
@@ -6,11 +6,14 @@ import tensorflow as tf
 class Pool(Protocol):
     """this is the pool of possible query candidates"""
 
+    shape: Tuple[int]
+    ranges: Tuple
+
     def __init__(self):
-        self.shape = None
+        ...
 
     def get_element_normalized(self, element: tf.Tensor) -> tf.Tensor:
-        pass
+        ...
 
     def get_shape(self):
         return self.shape
@@ -19,19 +22,19 @@ class Pool(Protocol):
         return tf.convert_to_tensor([self.get_element_normalized(x) for x in query_candidates])
 
     def get_elements(self, query_candidates: tf.Tensor) -> tf.Tensor:
-        pass
+        ...
 
     def normalize(self, query_candidates: tf.Tensor) -> tf.Tensor:
         return tf.map_fn(lambda x: self._normalize(x), query_candidates)
 
     def _normalize(self, query_candidate: tf.Tensor) -> tf.Tensor:
-        pass
+        ...
 
     def is_discrete(self) -> bool:
         return False
 
     def get_all_elements(self) -> List[tf.Tensor]:
-        pass
+        ...
 
     def get_elements_with_index(self, indices: tf.Tensor):
         return indices
